1d15ae0ab1079849498ac53274e50219ba41fd95
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-11 13:36:14 +0300

    Creating addLike API

--- laravel-server/app/Http/Controllers/LikeController.php
+++ laravel-server/app/Http/Controllers/LikeController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 use App\Models\Like;
 
 class LikeController extends Controller{
-    //
+    public function addLike(Request $request){
+        $like = new Like;
+        $like->user_id = $request->user_id;
+        $like->product_id = $request->product_id;
+        $like->save();
+        
+        return response()->json([
+            "status" => "Success"
+        ], 200);
+    }
 }
