0d532d65ff9000b4c80c9d437eb2e608937e7127
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-11 12:27:49 +0300

    Adding fillable array to product model

--- laravel-server/app/Models/Product.php
+++ laravel-server/app/Models/Product.php
@@ -5,7 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
-{
+class Product extends Model{
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'quantity',
+        'category_id',
+        'img_link', 
+        
+    ];
 }
