307dfe1dae689a7a986dfd18a698fdddb5767ea7
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-11 15:33:18 +0300

    Adding jwt authentication to product model

--- laravel-server/app/Http/Controllers/ProductController.php
+++ laravel-server/app/Http/Controllers/ProductController.php
@@ -1,11 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Validator;
 
 class ProductController extends Controller{
+      /**
+     * Create a new ProductController instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->middleware('auth:api', ['except' => ['addProduct']]);
+    }
     public function getAllProducts($id = null){
         if($id != null){
             $products = Product::find($id);
@@ -19,17 +28,57 @@ class ProductController extends Controller{
         ], 200);
     }
     public function addProduct(Request $request){
-        $product = new Product;
-        $product->name = $request->name;
-        $product->description = $request->description;
-        $product->price = $request->price;
-        $product->quantity = $request->quantity;
-        $product->category_id = $request->category_id;
-        $product->img_link = $request->img_link;
-        $product->save();
-        
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|between:2,100',
+            'description' => 'required|string',
+            'price' => 'required',
+            'quantity' =>'required|integer',
+            'category_id' =>'required|integer',
+            'img_link' =>'string',
+
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+        $product = Product::create(array_merge(
+                    $validator->validated()
+                ));
         return response()->json([
-            "status" => "Success"
-        ], 200);
+            'message' => 'product successfully added',
+            'product' => $product
+        ], 201);
+
     }
+        /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh() {
+        return $this->createNewToken(auth()->refresh());
+    }
+     /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function userProfile() {
+        return response()->json(auth()->user());
+    }
+     /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function createNewToken($token){
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'product' => auth()->product()
+        ]);
+}
+
 }
--- laravel-server/app/Models/Product.php
+++ laravel-server/app/Models/Product.php
@@ -1,11 +1,19 @@
 <?php
 
+// namespace App\Models;
+
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Model as Authenticatable;
+// use Illuminate\Notifications\Notifiable;
+// use Tymon\JWTAuth\Contracts\JWTSubject;
 namespace App\Models;
-
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Product extends Model{
+class Product extends Authenticatable implements JWTSubject{
     use HasFactory;
     protected $fillable = [
         'name',
@@ -16,4 +24,20 @@ class Product extends Model{
         'img_link', 
         
     ];
+        /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims() {
+        return [];
+    } 
 }
