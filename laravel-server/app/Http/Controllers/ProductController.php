b26e8491e85333d7bef936b51041773f45b1fc90
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-11 12:28:20 +0300

    Adding getallProducts API

--- laravel-server/app/Http/Controllers/ProductController.php
+++ laravel-server/app/Http/Controllers/ProductController.php
@@ -5,7 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 
-class ProductController extends Controller
-{
-    //
+class ProductController extends Controller{
+    public function getAllProducts($id = null){
+        if($id != null){
+            $products = Product::find($id);
+        }else{
+            $products = Product::all();
+        }
+        
+        return response()->json([
+            "status" => "Success",
+            "products" => $products
+        ], 200);
+    }
 }
