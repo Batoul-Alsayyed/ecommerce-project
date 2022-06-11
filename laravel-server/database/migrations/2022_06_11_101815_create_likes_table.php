ad394f7f14639d81e090d91be4b25475fb421da7
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-11 13:35:34 +0300

    Updating likes table

--- laravel-server/database/migrations/2022_06_11_101815_create_likes_table.php
+++ laravel-server/database/migrations/2022_06_11_101815_create_likes_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('likes', function (Blueprint $table) {
             $table->id();
-            $table->string('user_id');
-            $table->string('product_id');
+            $table->integer('user_id');
+            $table->integer('product_id');
             $table->timestamps();
         });
     }
