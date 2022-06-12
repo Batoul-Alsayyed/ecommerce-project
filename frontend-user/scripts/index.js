45c254c0210ace426980dcf792c1fb8df7888ed2
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-12 15:17:30 +0300

    Adding functionality for love button

--- frontend-user/scripts/index.js
+++ frontend-user/scripts/index.js
@@ -12,3 +12,12 @@ var menu_icon = document.getElementById("menu-icon");
 menu_icon.addEventListener("click", showMenu);
 var close_icon = document.getElementById("close-icon")
 close_icon.addEventListener("click",hideMenu);
+
+// var love_icon = document.getElementsByClassName("fa-heart");
+// love_icon.addEventListener("click", love);
+
+document.querySelectorAll('.fa-heart').forEach(item => {
+    item.addEventListener('click', event => {
+        item.style.color = "blueviolet";
+    })
+  })
