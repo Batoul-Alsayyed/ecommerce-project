0286f100eff69d8c4280b4ed4703922a71076696
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-13 06:19:02 +0300

    Adding dollar sin for prices and commenting testing commands

--- frontend-user/scripts/products.js
+++ frontend-user/scripts/products.js
@@ -1,5 +1,11 @@
 // Get products
 getProducts();
+document.querySelectorAll('.fa-heart').forEach(item => {
+    item.addEventListener('click', event => {
+        console.log("clicked");
+        item.style.color = "blueviolet";
+    })
+})
 let products = document.querySelector(".products");
 function getProducts(){
     axios({
@@ -7,20 +13,20 @@ function getProducts(){
         url: 'http://127.0.0.1:8000/api/products',
     })
     .then(function (response) {
-    console.log(response.data.products.length);
-    console.log(response.data.products[0]["price"]);
+    // console.log(response.data.products.length);
+    // console.log(response.data.products[0]["price"]);
 
     for (let product = 0; product<response.data.products.length; product++){
         let products_col = document.querySelector(".products-col");
         let row = document.querySelector(".row");
 
-        console.log(product);
+        // console.log(product);
         if (product===0){
             products_col.innerHTML = `<div class="products-col">
             <img src="../assets/products/charisse-kenion-dah-jZWgzx8-unsplash.jpg" alt="">
             <h3>${response.data.products[product]["name"]} <i class="fa-solid fa-heart"></i></h3>
             <p>${response.data.products[product]["description"]}</p>
-            <p>${response.data.products[product]["price"]}</p>
+            <p>${response.data.products[product]["price"]}$</p>
         </div>`
         }
         else{
@@ -29,7 +35,7 @@ function getProducts(){
                 <img src="../assets/products/charisse-kenion-dah-jZWgzx8-unsplash.jpg" alt="">
                 <h3>${response.data.products[product]["name"]} <i class="fa-solid fa-heart"></i></h3>
                 <p>${response.data.products[product]["description"]}</p>
-                <p>${response.data.products[product]["price"]}</p>
+                <p>${response.data.products[product]["price"]}$</p>
             </div>`
             }
             else{
@@ -37,7 +43,7 @@ function getProducts(){
                 <img src="../assets/products/charisse-kenion-dah-jZWgzx8-unsplash.jpg" alt="">
                 <h3>${response.data.products[product]["name"]} <i class="fa-solid fa-heart"></i></h3>
                 <p>${response.data.products[product]["description"]}</p>
-                <p>${response.data.products[product]["price"]}</p>
+                <p>${response.data.products[product]["price"]}$</p>
             </div>`
             }
         }
@@ -48,3 +54,12 @@ function getProducts(){
     
 })
 }
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('.fa-heart').forEach(item => {
+        item.addEventListener('click', event => {
+            console.log("clicked");
+            item.style.color = "blueviolet";
+        })
+    })
+});
