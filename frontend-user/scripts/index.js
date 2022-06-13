3a30179f7e05af4e786eb6dd0a898de09805b629
Batoul <105310715+batoulalsayyed@users.noreply.github.com>
2022-06-13 05:59:33 +0300

    Getting user id from local storage

--- frontend-user/scripts/index.js
+++ frontend-user/scripts/index.js
@@ -21,3 +21,6 @@ document.querySelectorAll('.fa-heart').forEach(item => {
         item.style.color = "blueviolet";
     })
   })
+
+let user_id = localStorage.getItem("user_id");  
+console.log('user_id: ',user_id);
